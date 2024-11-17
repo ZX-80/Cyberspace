23e0eedfd4f696c13355c0ff6d425ad4e4a9a40e
Jefferson <44975876+ZX-80@users.noreply.github.com>
2024-11-17 16:18:34 +0000

    Fix licenses for 3rd party software

--- src/cyberspace_ssg/filters/mermaid.py
+++ src/cyberspace_ssg/filters/mermaid.py
@@ -50,7 +50,9 @@ def detect_mermaid(elem: pf.Element, doc: pf.Doc) -> pf.RawBlock | None:
 def finalize(doc: pf.Doc) -> None:
     """Insert mermaid script if necessary"""
     if doc.metadata.get("mermaid"):
-        doc.content.append(pf.RawBlock(str(dom.script(src=JS_PATH / "mermaid.esm.min.mjs", type="module"))))
+        doc.content.append(
+            pf.RawBlock(str(dom.script(src=JS_PATH / "mermaid_js" / "mermaid.esm.min.mjs", type="module")))
+        )
 
 
 def main(doc: pf.Doc | None = None) -> pf.Doc | None:
