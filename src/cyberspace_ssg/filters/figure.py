734e772bb18803bc75665f759d1cf3dd70613870
Jefferson <44975876+ZX-80@users.noreply.github.com>
2024-12-14 20:58:59 +0400

    captions

---
+++ src/cyberspace_ssg/filters/figure.py
@@ -0,0 +1,31 @@
+"""
+Allow captions anywhere.
+
+![Image](link)
+^ image caption
+"""
+
+import panflute as pf
+
+
+def wrap_caption(elem: pf.Element, _doc: pf.Doc) -> pf.Figure | None:
+    """Wrap elements in a figure if a caption is present."""
+    if (
+        isinstance(paragraph := elem.next, pf.Para)
+        and elem.next.content
+        and isinstance(string := elem.next.content[0], pf.Str)
+        and string.text.startswith("^ ")
+    ):
+        string.text = string.text.removeprefix("^ ")
+        del elem.parent.content[elem.index + 1]  # Remove the caption object
+        return pf.Figure(elem, caption=pf.Caption(paragraph))
+    return None
+
+
+def main(doc: pf.Doc | None = None) -> pf.Doc | None:
+    """Run document through some filters."""
+    return pf.run_filter(wrap_caption, doc=doc)
+
+
+if __name__ == "__main__":
+    main()
